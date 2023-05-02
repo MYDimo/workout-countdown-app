9c813d8e92ef573882ce94fd0393aa0111bbc975
Mihaylo Dimo <mihaylo.dimo@gmail.com>
2023-05-03 00:30:10 +0300

    Fix lint issues preventing Netlify to deploy website #2

--- src/components/ClockTool.js
+++ src/components/ClockTool.js
@@ -3,19 +3,6 @@ import React, { useState, useEffect } from "react";
 export default function ClockTool({ toggleClock }) {
 	const [currentTime, setTime] = useState(`00:00:00`);
 
-	const animate = () => {
-		const date = new Date();
-		const hours = String(date.getHours());
-		const minutes = String(date.getMinutes());
-		const seconds = String(date.getSeconds());
-
-		const time = `${hours.padStart(2, "0")}:${minutes.padStart(2, "0")}:${seconds.padStart(2, "0")}`;
-
-		setTime(time);
-
-		requestAnimationFrame(animate);
-	};
-
 	useEffect(() => {
     const animate = () => {
       const date = new Date();

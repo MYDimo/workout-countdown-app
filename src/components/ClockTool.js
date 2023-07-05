1d4236e8b7f7c7862fdb509a30ab12f0cf166a82
Mihaylo Dimo <mihaylo.dimo@gmail.com>
2023-07-05 23:00:10 +0300

    Fix bug - when clock tool closed it was still running

--- src/components/ClockTool.js
+++ src/components/ClockTool.js
@@ -5,6 +5,8 @@ export default function ClockTool({ toggleClock }) {
 	const [currentTime, setTime] = useState(`00:00:00`);
 
 	useEffect(() => {
+		let animationId;
+
     const animate = () => {
       const date = new Date();
       const hours = String(date.getHours());
@@ -12,19 +14,29 @@ export default function ClockTool({ toggleClock }) {
       const seconds = String(date.getSeconds());
   
       const time = `${hours.padStart(2, "0")}:${minutes.padStart(2, "0")}:${seconds.padStart(2, "0")}`;
+			console.log(time);
   
       setTime(time);
   
-      requestAnimationFrame(animate);
+      animationId = requestAnimationFrame(animate);
     };
+		
+		 animationId = requestAnimationFrame(animate);
 
-		requestAnimationFrame(animate);
+		return function cleanup() {
+			cancelAnimationFrame(animationId);
+			console.log("animation stopped");
+		}
+   	//cancel animation with return callback??
+
 	}, []);
 
 	return (
+		<>
 		<div className="toolBody">
 			<CloseIcon className="closeTool" onClick={toggleClock} alt="Back to main menu"/>
 			<h1>{currentTime}</h1>
 		</div>
+		</>
 	);
 }
